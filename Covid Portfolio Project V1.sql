6f854b4e4bd1fe8541a21a7cf232bac6756603bd
Andrew Piao <96275469+FrankieD6@users.noreply.github.com>
2023-03-25 20:36:39 -0400

    Update Covid Portfolio Project V1.sql

--- Covid Portfolio Project V1.sql
+++ Covid Portfolio Project V1.sql
@@ -117,3 +117,48 @@ WHERE dea.continent is not null
 
 SELECT *
 FROM Percent_Population_Vaccinated
+
+
+/*
+
+Queries used for Tableau Project
+
+*/
+
+
+
+--Creating table for 'Global Numbers' to be pasted into Excel then imported into Tableau
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From [Portfolio Project 01]..CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+--Creating table for 'Death Count Per Continent' to be pasted into Excel then imported into Tableau
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From [Portfolio Project 01]..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+--Creating table for 'Highest Infection Count and Percentage Per Country' to be pasted into Excel then imported into Tableau
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From [Portfolio Project 01]..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+--Creating table for 'Infection Count and Percentage Per Country Per Day' to be pasted into Excel then imported into Tableau
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From [Portfolio Project 01]..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
